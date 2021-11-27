044e883bcfb8ffc752b34a390431a258fa2173e2
Chenyan_Zhang <24949855@qq.com>
2021-11-28 02:22:36 +0800

    upgrade V1.1

--- Random_Walk_2d.py
+++ Random_Walk_2d.py
@@ -1,6 +1,8 @@
 import random
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
 def random_walk(n):
     x = 0
     y = 0
@@ -16,13 +18,16 @@ def random_walk(n):
             x -= 1
     return x, y
 
+
 # 坐标点的展示
-coordinate= pd.DataFrame({'x轴':[0],'y轴':[0]})
+coordinate = pd.DataFrame({'x轴': [0], 'y轴': [0]})
 for i in range(100):
     step_walk = random_walk(20)
     coordinate.loc[i] = step_walk  # 对df新增行时，使用loc[i]。不能使用iloc[i]
+    # coordinate = coordinate.append(step_walk,ignore_index=True) 另一种在df最后添加一行的方法，但step_walk需要为df
+
 x = coordinate['x轴']
 y = coordinate['y轴']
-plt.plot(x,y,'*')
+plt.plot(x, y, '*')
 plt.show()
 
